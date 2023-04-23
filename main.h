88d13f6a463cd0aa7c6921faf82a44a33442d1a9
Hatimsel <selmun13@gmail.com>
2023-04-23 12:29:02 +0000

    main.h

--- main.h
+++ main.h
@@ -2,8 +2,17 @@
 #define _MAIN_H_
 #include <unistd.h>
 
+#define _MINUS 1
+#define _PLUS 2
+#define _ZERO 4
+#define _HASH 8
+#define _SPACE 16
+
+/*int binary(const char *format, ...);*/
 int _printf(const char *format, ...);
 ssize_t write(int fd, const void *buf, size_t count);
+int flags(const char *format, ...);
+
 
 
 #endif
