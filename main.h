1b88990a3d21b9bb19cc5b79c86f2e0149d7c729
Hatimsel <selmun13@gmail.com>
2023-04-23 16:41:19 +0000

    header

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <unistd.h>
+#include <stdarg.h>
+
+#define BUFFER_SIZE 1024
 
 #define _MINUS 1
 #define _PLUS 2
@@ -8,11 +11,22 @@
 #define _HASH 8
 #define _SPACE 16
 
+/*function prototypes*/
+
 /*int binary(const char *format, ...);*/
 int _printf(const char *format, ...);
 ssize_t write(int fd, const void *buf, size_t count);
 int flags(const char *format, ...);
+int width(const char *format, int *pos, va_list args);
+int precision(const char *format, int start_pos, va_list args);
 
+/*function helpers*/
+
+int is_alpha(int c);
+int is_digit(char);
+
+#define length_SHORT 1
+#define length_LONG 2
 
 
 #endif
