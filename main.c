c7bf4fe2b1c8c79cb838ef222f1ab9832b8f4047
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-04-25 05:45:09 +0000

    final_project

--- main.c
+++ main.c
@@ -1,44 +1,51 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - Entry point
- *
+ * This is our test file for our _printf function
  * Return: Always 0
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (0);
+	printf("STANDAR PRINTF FUNCTION OUTPUT'S\n\n");
+	len2 = printf("This sentence is printed with the standar printf.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	printf("Length (using '%%i' and '%%d'):  [%d, %i]\n", len2, len2);
+	printf("Negative number (using '%%i' or '%%d'):  [%d]\n", -762534);
+	printf("Unsigned (using (using '%%u'):  [%u]\n", ui);
+	printf("Unsigned octal (using '%%o'):  [%o]\n", ui);
+	printf("Unsigned hexadecimal (using '%%x' and '%%X'): [%x, %X]\n", ui, ui);
+	printf("Character (using '%%c'):  [%c]\n", 'H');
+	printf("String (using '%%s'):  [%s]\n", "I am a string !");
+	printf("Address (using '%%p'):  [%p]\n", addr);
+	len2 = printf("Percent (using '%%%%'):  [%%]\n");
+	printf("Len (using '%%d'):  [%d]\n", len2);
+	printf("Binary (using '%%b'):  [%b]\n", 98);
+	printf("***There is no format specifier in 'C' for [%%r] [%%S] ***\n");
+	printf("\n");
+	printf("OUR UPDATED PRINTF FUNCTION'S OUTPUT (_PRINTF)\n\n");
+	len = _printf("This sentence is printed with our _printf.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	_printf("Length (using '%%i' and '%%d'):  [%d, %i]\n", len, len);
+	_printf("Negative (using '%%i' and '%%d'):  [%d]\n", -762534);
+	_printf("Unsigned (using '%%u'):  [%u]\n", ui);
+	_printf("Unsigned octal (using '%%o'):  [%o]\n", ui);
+	_printf("Unsigned hexadecimal (using '%%x' and '%%X): [%x, %X]\n", ui, ui);
+	_printf("Character (using '%%c'):  [%c]\n", 'H');
+	_printf("String (using '%%s'):  [%s]\n", "I am a string !");
+	_printf("Address (using '%%p'):  [%p]\n", addr);
+	len = _printf("Percent (using '%%%%'):  [%%]\n");
+	_printf("Len (using '%%d'):  [%d]\n", len);
+	_printf("Binary (using '%%b'):  [%b]\n", 98);
+	_printf("Reverse (using '%%r'):  [%r]\n", "I'm a string in reverse");
+	_printf("Forbiden ASCII (using '%%S): %S\n", "Best\nSchool");
+	return (0);
 }
